c46fafb37e5956b7c1d43d82caee6cc93f33910b
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-05-25 22:14:25 +0900

    impl: detect_platform_from_url()

--- webclient/src/lib.rs
+++ webclient/src/lib.rs
@@ -17,5 +17,34 @@ pub fn new_client(platform: Platform) -> Box<dyn Client> {
     }
 }
 
+/// ```
+/// use kpr_webclient::{detect_platform, Platform, Url};
+///
+/// let platform = detect_platform("https://atcoder.jp");
+/// assert_eq!(platform, Some(Platform::AtCoder));
+///
+/// let platform = detect_platform("https://atcoder.jp/contests/abc001/tasks/");
+/// assert_eq!(platform, Some(Platform::AtCoder));
+///
+/// let platform = detect_platform("https://example.com");
+/// assert_eq!(platform, None);
+/// ```
+pub fn detect_platform(url: impl TryInto<Url>) -> Option<Platform> {
+    url.try_into()
+        .ok()
+        .as_ref()
+        .and_then(detect_platform_from_url)
+}
+
+pub fn detect_platform_from_url(url: &Url) -> Option<Platform> {
+    let Some(domain) = url.domain() else {
+        return None
+    };
+    match domain {
+        atcoder::DOMAIN => Some(Platform::AtCoder),
+        _ => None,
+    }
+}
+
 // internal modules
 mod util;
