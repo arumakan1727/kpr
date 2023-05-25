a2724d159daffde80748bbd40e66cd0bf711367b
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-05-26 00:53:51 +0900

    fix(subcmd): Cancel init if cwd is already in kpr-repository

--- kpr/src/cmd/init.rs
+++ kpr/src/cmd/init.rs
@@ -1,5 +1,7 @@
-use kpr_core::action;
+use kpr_core::{action, config::RepoConfig};
 use std::{path::PathBuf, process::exit};
+
+use crate::util;
 
 use super::GlobalArgs;
 
@@ -10,6 +12,15 @@ pub struct Args {
 }
 
 pub fn exec(args: &Args, _: &GlobalArgs) -> ! {
+    if let Some(repo_config_path) = RepoConfig::find_filepath(util::current_dir()) {
+        eprintln!(
+            "Current dir is already in kpr-repository. (config path: '{}')",
+            repo_config_path.to_string_lossy()
+        );
+        eprintln!("Canceled initializing.");
+        exit(1);
+    }
+
     action::init_kpr_repository(&args.dir).unwrap_or_else(|e| {
         eprintln!("Failed to init kpr repository: {}", e);
         exit(1);
