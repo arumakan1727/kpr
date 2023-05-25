9a32439af4654702bb01ac221b800995080ddd40
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-05-25 14:36:32 +0900

    add: config::KprRepository

--- core/src/config.rs
+++ core/src/config.rs
@@ -1,5 +1,106 @@
+use std::path::{Path, PathBuf};
+use std::result::Result as StdResult;
+
+use anyhow::{bail, Context};
 use kpr_webclient::Platform;
+use rust_embed::RustEmbed;
+use serde::Deserialize;
+
+use crate::storage;
+
+pub const PROBLEM_URL_FILENAME: &str = ".url.txt";
+pub const REPOSITORY_CONFIG_FILENAME: &str = "kpr-repository.toml";
 
 pub fn authtoken_filename(platform: Platform) -> String {
     format!("{}-auth.json", platform.lowercase())
+}
+
+/// Returns tuple (input_filename, output_filename).
+///
+/// ```
+/// use kpr_core::config::testcase_filename;
+///
+/// let (infile, outfile) = testcase_filename(1);
+/// assert_eq!(infile, "in1.txt");
+/// assert_eq!(outfile, "out1.txt");
+/// ```
+pub fn testcase_filename(ord: u32) -> (String, String) {
+    (format!("in{}.txt", ord), format!("out{}.txt", ord))
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct KprRepository {
+    #[serde(skip)]
+    pub root_dir: PathBuf,
+
+    pub vault_home: PathBuf,
+    pub daily_home: PathBuf,
+    pub solvespace_template: PathBuf,
+}
+
+#[derive(RustEmbed)]
+#[folder = "examples/assets/"]
+struct Asset;
+
+impl KprRepository {
+    pub fn example_toml() -> String {
+        let file = Asset::get(REPOSITORY_CONFIG_FILENAME).unwrap();
+        std::str::from_utf8(file.data.as_ref()).unwrap().to_owned()
+    }
+
+    pub fn from_toml(s: &str) -> StdResult<Self, toml::de::Error> {
+        toml::from_str(s)
+    }
+
+    /// Find REPOSITORY_CONFIG_FILENAME from current_dir to ancestor dirs.
+    pub fn find_repoconfig_path(current_dir: impl AsRef<Path>) -> Option<PathBuf> {
+        assert!(current_dir.as_ref().is_absolute());
+
+        let mut current_dir = Some(current_dir.as_ref());
+        while let Some(dir) = current_dir {
+            let filepath = dir.join(REPOSITORY_CONFIG_FILENAME);
+            if filepath.is_file() {
+                return Some(filepath.to_owned());
+            }
+            current_dir = dir.parent();
+        }
+        None
+    }
+
+    pub fn from_fs(current_dir: impl AsRef<Path>) -> anyhow::Result<Self> {
+        let Some(config_path) = Self::find_repoconfig_path(current_dir) else {
+            bail!("Not in a kpr-repository dir: '{}' not found.", REPOSITORY_CONFIG_FILENAME);
+        };
+        let contents = storage::util::read_to_string(&config_path).context("Failed to read")?;
+        let mut r = Self::from_toml(&contents)
+            .with_context(|| format!("Invalid TOML: {}", config_path.to_string_lossy()))?;
+        r = r.with_root_dir(config_path.parent().unwrap());
+        Ok(r)
+    }
+
+    pub fn with_root_dir(mut self, root: impl AsRef<Path>) -> Self {
+        let root = root.as_ref();
+        self.root_dir = root.to_owned();
+        self.vault_home = root.join(self.vault_home);
+        self.daily_home = root.join(self.daily_home);
+        self.solvespace_template = root.join(self.solvespace_template);
+        self
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example_toml_should_be_parsable() {
+        let toml = KprRepository::example_toml();
+        let r = KprRepository::from_toml(&toml).unwrap_or_else(|e| {
+            panic!("{}", e);
+        });
+        assert_eq!(r.root_dir, Path::new(""));
+        assert_eq!(r.vault_home, Path::new("./vault"));
+        assert_eq!(r.daily_home, Path::new("./daily"));
+        assert_eq!(r.solvespace_template, Path::new("./template"));
+    }
 }
