8494c47ed8f17a0cbf8a41e7eb7dd3cb53b7b035
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-06-04 19:18:06 +0900

    refactor: extract logic to util::determine_program_file()

--- cli/src/util.rs
+++ cli/src/util.rs
@@ -5,6 +5,9 @@ use std::{
     process::exit,
 };
 
+use anyhow::{bail, Context};
+use kpr_core::{fsutil, serdable::GlobPattern};
+
 pub fn dedup<T>(mut v: Vec<T>) -> Vec<T>
 where
     T: Hash + Eq + Copy,
@@ -29,4 +32,22 @@ pub fn replace_homedir_to_tilde(path: impl Into<PathBuf>) -> PathBuf {
     path.strip_prefix(home_dir)
         .map(|path| Path::new("~").join(path))
         .unwrap_or(path)
+}
+
+pub fn determine_program_file(
+    program_file_or_dir: &Option<PathBuf>,
+    file_pattern: &GlobPattern,
+) -> anyhow::Result<PathBuf> {
+    let existing_path = match program_file_or_dir {
+        Some(path) if path.exists() => path,
+        Some(path) => bail!("No such file or dir: {:?}", path),
+        None => Path::new("./"),
+    };
+
+    if existing_path.is_dir() {
+        fsutil::find_most_recently_modified_file(&existing_path, &file_pattern)
+            .with_context(|| format!("Cannot find target program file in {:?}", existing_path))
+    } else {
+        Ok(existing_path.to_owned())
+    }
 }
