d69d07dc5d1ca3f03d9992856db1ac8396894a25
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-07-09 14:35:31 +0900

    refactor: expander::cpp

--- expander/src/cpp/expander.rs
+++ expander/src/cpp/expander.rs
@@ -21,62 +21,56 @@ static RE_INCLUDE_ANGLE_BRA: lazy_regex::Lazy<Regex> = lazy_regex!(r#"^\s*#\s*in
 static RE_INCLUDE_DBL_QUOTE: lazy_regex::Lazy<Regex> = lazy_regex!(r#"^\s*#\s*include\s*"(.+)""#);
 static RE_PRAGMA_ONCE: lazy_regex::Lazy<Regex> = lazy_regex!(r#"\s*#\s*pragma\s+once"#);
 
-#[derive(Debug, Clone)]
-pub struct Expander {
-    header_serch_dirs: Vec<PathBuf>,
-    expansion_targets: Vec<Pattern>,
-    black_list: Vec<Pattern>,
+fn extract_include_argument(line: &str) -> Option<(String, HeaderSearchMode)> {
+    RE_INCLUDE_ANGLE_BRA
+        .captures(line)
+        .and_then(|cap| Some((cap[1].trim().to_owned(), HeaderSearchMode::OnlyIncludePath)))
+        .or_else(|| {
+            RE_INCLUDE_DBL_QUOTE
+                .captures(line)
+                .map(|cap| (cap[1].trim().to_owned(), HeaderSearchMode::CurrentDirFirst))
+        })
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Expander<'a> {
+    header_serch_dirs: &'a [PathBuf],
+    expansion_targets: &'a [GlobPattern],
+    black_list: &'a [GlobPattern],
 
     // [(literal_header_path, mode, header_full_path)]
     include_directive_occurrences: Vec<(String, HeaderSearchMode, PathBuf)>,
     expanded_header_abs_paths: HashSet<PathBuf>,
-    includes_bits_stdcpp_h: bool,
+    found_bits_stdcpp_h: bool,
     generated_code: String,
 }
 
-#[derive(Debug, Clone, Default)]
-pub struct ExpanderBuilder {
-    header_serch_dirs: Vec<PathBuf>,
-    expansion_targets: Vec<Pattern>,
-    black_list: Vec<Pattern>,
-}
+impl<'a> Expander<'a> {
+    pub fn new() -> Self {
+        Self::default()
+    }
 
-impl ExpanderBuilder {
-    pub fn header_serch_dirs(mut self, v: Vec<PathBuf>) -> Self {
+    pub fn header_serch_dirs(mut self, v: &'a [PathBuf]) -> Self {
         self.header_serch_dirs = v;
         self
     }
-    pub fn expansion_targets(mut self, v: Vec<Pattern>) -> Self {
+    pub fn expansion_targets(mut self, v: &'a [GlobPattern]) -> Self {
         self.expansion_targets = v;
         self
     }
-    pub fn black_list(mut self, v: Vec<Pattern>) -> Self {
+    pub fn black_list(mut self, v: &'a [GlobPattern]) -> Self {
         self.black_list = v;
         self
     }
+
     pub fn expand(
-        self,
+        mut self,
         source_code_dir: impl AsRef<Path>,
         source_code: impl AsRef<str>,
     ) -> anyhow::Result<String> {
         let dir = fsutil::canonicalize_path(source_code_dir)?;
-        let e = Expander {
-            header_serch_dirs: self.header_serch_dirs,
-            expansion_targets: self.expansion_targets,
-            black_list: self.black_list,
-
-            includes_bits_stdcpp_h: false,
-            include_directive_occurrences: Vec::with_capacity(128),
-            expanded_header_abs_paths: HashSet::with_capacity(16),
-            generated_code: String::with_capacity(source_code.as_ref().len()),
-        };
-        Ok(e.expand(dir, source_code).get_generated_code())
-    }
-}
-
-impl Expander {
-    pub fn builder() -> ExpanderBuilder {
-        ExpanderBuilder::default()
+        self.emit(dir, source_code);
+        Ok(self.get_generated_code())
     }
 
     pub fn get_generated_code(&self) -> String {
@@ -85,7 +79,7 @@ impl Expander {
         let mut included_header_abs_paths: HashSet<&Path> =
             HashSet::with_capacity(self.include_directive_occurrences.len());
 
-        if self.includes_bits_stdcpp_h {
+        if self.found_bits_stdcpp_h {
             s += "#include <bits/stdc++.h>\n";
             included_header_abs_paths.extend(BITS_STDCPP_H_SORTED_HEADERS.iter().map(Path::new));
         }
@@ -110,17 +104,6 @@ impl Expander {
         s
     }
 
-    fn extract_include_argument(line: &str) -> Option<(String, HeaderSearchMode)> {
-        RE_INCLUDE_ANGLE_BRA
-            .captures(line)
-            .and_then(|cap| Some((cap[1].trim().to_owned(), HeaderSearchMode::OnlyIncludePath)))
-            .or_else(|| {
-                RE_INCLUDE_DBL_QUOTE
-                    .captures(line)
-                    .map(|cap| (cap[1].trim().to_owned(), HeaderSearchMode::CurrentDirFirst))
-            })
-    }
-
     pub fn may_expand(&self, literal_header_path: impl AsRef<str>, mode: HeaderSearchMode) -> bool {
         let literal_header_path = literal_header_path.as_ref();
 
@@ -143,7 +126,7 @@ impl Expander {
             .any(|pat| pat.matches(literal_header_path));
     }
 
-    pub fn expand(mut self, abs_cwd: impl AsRef<Path>, source_code: impl AsRef<str>) -> Self {
+    pub fn emit(&mut self, abs_cwd: impl AsRef<Path>, source_code: impl AsRef<str>) -> () {
         let source_code = source_code.as_ref();
         let abs_cwd = abs_cwd.as_ref();
 
@@ -152,7 +135,7 @@ impl Expander {
                 continue;
             }
 
-            let Some((literal_header_path, mode)) = Self::extract_include_argument(line) else {
+            let Some((literal_header_path, mode)) = self::extract_include_argument(line) else {
                 self.generated_code += line;
                 self.generated_code.push('\n');
                 continue;
@@ -165,7 +148,7 @@ impl Expander {
             ));
 
             if literal_header_path == "bits/stdc++.h" {
-                self.includes_bits_stdcpp_h = true;
+                self.found_bits_stdcpp_h = true;
                 continue;
             }
             if !self.may_expand(&literal_header_path, mode) {
@@ -187,14 +170,14 @@ impl Expander {
                         self.include_directive_occurrences.last_mut().unwrap().2 =
                             normalized_header_path.clone();
 
-                        self = self.expand(normalized_header_path.parent().unwrap(), content);
+                        self.emit(normalized_header_path.parent().unwrap(), content);
                         continue;
                     }
                     _ => (),
                 }
             }
 
-            for dir in &self.header_serch_dirs {
+            for dir in self.header_serch_dirs {
                 let (status, normalized_header_path) =
                     self.check_expansion(dir, &literal_header_path);
 
@@ -207,14 +190,13 @@ impl Expander {
                         self.include_directive_occurrences.last_mut().unwrap().2 =
                             normalized_header_path.clone();
 
-                        self = self.expand(normalized_header_path.parent().unwrap(), content);
+                        self.emit(normalized_header_path.parent().unwrap(), content);
                         continue 'line_loop;
                     }
                     NoSuchHeaderFile => (),
                 }
             }
         }
-        self
     }
 
     /// Returns (expansion_status, normalized_header_path)
@@ -261,7 +243,7 @@ mod test {
 
     #[test]
     fn should_be_ok_with_no_config_with_bits_stdcpp_h() {
-        let generated = Expander::builder()
+        let generated = Expander::default()
             .expand(
                 std::env::current_dir().unwrap(),
                 r#"#include <iostream>
@@ -314,7 +296,7 @@ int main() {
 
     #[test]
     fn should_be_ok_with_no_config_without_bits_stdcpp_h() {
-        let generated = Expander::builder()
+        let generated = Expander::default()
             .expand(
                 std::env::current_dir().unwrap(),
                 r#"#include <iostream>
